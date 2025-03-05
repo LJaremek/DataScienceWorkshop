8a2c44ec503654933c28f71659785a9240d38794
Łukasz Jaremek <jaremek.lukasz@gmail.com>
2025-03-05 19:29:52 +0100

    add simple but working code

---
+++ scrap_google_maps.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1200,800")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+try:
+    driver.get("https://www.google.pl/maps/@52.2195197,21.0102444,101m/data=!3m1!1e3?entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D")
+
+    time.sleep(5)
+
+    try:
+        accept_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Zaakceptuj wszystko')]"))
+        )
+        accept_button.click()
+        print("Kliknięto 'Zaakceptuj wszystko'.")
+        time.sleep(3)
+    except Exception as e:
+        print("Nie znaleziono przycisku zgód lub już zaakceptowane.")
+
+    screenshot_path = "google_maps_screenshot.png"
+    driver.save_screenshot(screenshot_path)
+    print(f"Screenshot zapisany jako: {screenshot_path}")
+
+finally:
+    driver.quit()
